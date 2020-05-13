cda636206dda41363105696f24a5822cc1e8f9b9
Vanessa Mercado <vanessa.mercado24@gmail.com>
2020-05-13 14:12:44 -0700

    added form

--- src/App.js
+++ src/App.js
@@ -4,12 +4,41 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import TodosList from "./components/TodosList";
 import EditTodo from "./components/EditTodo";
 import CreateTodo from "./components/CreateTodo";
+import Logo from "./logo.png";
 
 const App = () => {
   return (
     <Router>
       <div className="container">
-        <h1>MERN-Stack Todo App</h1>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          {/* <a className="navbar-brand"> */}
+          <img
+            className="navbar-brand"
+            src={Logo}
+            width="30"
+            height="30"
+            alt="logo"
+          />
+          {/* </a> */}
+          <Link to="/" className="navbar-brand">
+            MERN_STACK Todo App
+          </Link>
+          <div className="collapase nav-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <Link to="/" className="nav-link">
+                  Todos
+                </Link>
+              </li>
+              <li>
+                <Link to="/create" className="nav-link">
+                  Create Todo
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
         <Route path="/" exact component={TodosList} />
         <Route path="/edit/:id" component={EditTodo} />
         <Route path="/create" component={CreateTodo} />
--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -1,9 +1,77 @@
-import React, { UseState } from "react";
+import React, { UseState, useState, useEffect } from "react";
+
+function createNewTodo() {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 1000);
+  });
+}
+const initialState = {
+  description: "",
+  responsible: "",
+  priority: "",
+  isCompleted: false,
+};
 
 const CreateTodo = () => {
+  const [
+    { description, responsible, priority, isCompleted },
+    setState,
+  ] = useState(initialState);
+
+  const clearState = () => {
+    setState({ ...initialState });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setState((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createNewTodo().then(clearState);
+    console.log(`Description: ${description}`);
+    console.log(`responsible: ${responsible}`);
+    console.log(`priority: ${priority}`);
+    console.log(`isCompleted: ${isCompleted}`);
+  };
+
   return (
-    <div>
-      <p>Welcome to the CreateTodo Component</p>
+    <div style={{ marginTop: 20 }}>
+      <h3>Create New Todo</h3>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Description: </label>
+          <input
+            name="description"
+            value={description}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Responsible: </label>
+
+          <input
+            value={responsible}
+            name="responsible"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Priority: </label>
+          <input value={priority} name="priority" onChange={handleChange} />
+        </div>
+        <div className="form-group">
+          <label>Completed: </label>
+          <input
+            checked={isCompleted}
+            name="isCompleted"
+            type="checkbox"
+            onChange={handleChange}
+          />
+        </div>
+        <input type="submit" value="Save" />
+      </form>
     </div>
   );
 };
